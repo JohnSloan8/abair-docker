c6e4ec6e2af46f430b8168b4a923f1efbf7deaf7
JohnSloan8 <johnsloan1984@gmail.com>
2023-02-13 15:39:35 +0000

    sso working

--- abair/dist/assets/index.3ddf7c07.js
+++
@@ -1,1 +0,0 @@
-import{R as s,d as e}from"./setPrototypeOf.fd4aa278.js";const o=s({key:"front-page-tabs-state",default:0}),r=()=>{const[a,t]=e(o);return{frontPageTabs:a,setFrontPageTabs:t}},n=s({key:"app-tabs-state",default:0}),b=()=>{const[a,t]=e(n);return{appTabs:a,setAppTabs:t}};export{b as a,r as u};
