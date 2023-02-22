a9807bed6346afe4a8c1e00689c1842aa74651db
john <johnsloan1984@gmail.com>
2023-02-22 13:19:54 +0000

    redid error-check paths

--- error-check/dist/run-server.js
+++ error-check/dist/run-server.js
@@ -6,12 +6,12 @@ const host = "localhost";
 const port = 8002;
 app.use(express.json());
 app.use(cors());
-app.post("/api/error-check/run-full-check", function (req, res) {
+app.post("/run-full-check", function (req, res) {
     const { word, target } = req.body;
     const errorCheckOutput = runErrorCheck(word, target);
     res.send(`${JSON.stringify(errorCheckOutput)}`);
 });
-app.get("/api/error-check/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Welcome to Error Check!");
 });
 app.listen(port, function (error) {
