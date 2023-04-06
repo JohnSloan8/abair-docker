94188c79f2a811c9b2a0662bb5709309d5cf4e90
john <johnsloan1984@gmail.com>
2023-04-06 12:00:22 +0100

    got paths right for error-check

--- error-check/dist/run-server.js
+++ error-check/dist/run-server.js
@@ -6,14 +6,14 @@ app.use(cors());
 const host = "localhost";
 const port = 8002;
 import runErrorCheck from "./runErrorCheck.js";
-app.post("/apis/error-check/run-full-check", function (req, res) {
+app.post("/run-full-check", function (req, res) {
     const { word, target } = req.body;
     const errorCheckOutput = runErrorCheck(word, target);
     const responseData = `${JSON.stringify(errorCheckOutput)}`;
     console.log("responseData", responseData);
     res.send(responseData);
 });
-app.get("/apis/error-check", (req, res) => {
+app.get("/", (req, res) => {
     console.log("in home");
     res.send("Welcome to Error Check!");
 });
