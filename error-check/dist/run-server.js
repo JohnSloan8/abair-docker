dbdee59cc82baece39eb6cf70d3b6ef3345d012f
john <johnsloan1984@gmail.com>
2023-04-06 11:57:15 +0100

    changed error-check path

--- error-check/dist/run-server.js
+++ error-check/dist/run-server.js
@@ -6,15 +6,15 @@ app.use(cors());
 const host = "localhost";
 const port = 8002;
 import runErrorCheck from "./runErrorCheck.js";
-app.post("/run-full-check", function (req, res) {
+app.post("/apis/error-check/run-full-check", function (req, res) {
     const { word, target } = req.body;
     const errorCheckOutput = runErrorCheck(word, target);
     const responseData = `${JSON.stringify(errorCheckOutput)}`;
-    console.log('responseData', responseData);
+    console.log("responseData", responseData);
     res.send(responseData);
 });
-app.get("/", (req, res) => {
-    console.log('in home');
+app.get("/apis/error-check", (req, res) => {
+    console.log("in home");
     res.send("Welcome to Error Check!");
 });
 app.listen(port, function (error) {
