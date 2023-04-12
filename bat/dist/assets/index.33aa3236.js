a504de6b7e8fe6270ccddd1242650a11c93a95ed
john <johnsloan1984@gmail.com>
2023-04-12 11:57:55 +0100

    updated modal on bat

---
+++ bat/dist/assets/index.33aa3236.js
@@ -0,0 +1,1 @@
+import{C as r}from"./App.eb3f81e0.js";import{C as t}from"./ChatHistories.3a35cccb.js";import{j as o}from"./setPrototypeOf.1b27e896.js";import"./index.53260034.js";import"./index.f8d7f652.js";import"./TotalPoints.9d5f9d66.js";const s=()=>o(r,{mt:2,children:o(t,{showHowMany:1e3})}),f=s;export{f as default};
--- bat/dist/assets/index.f5833402.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./App.aab192d0.js";import{C as t}from"./ChatHistories.4f9abbeb.js";import{j as o}from"./setPrototypeOf.1b27e896.js";import"./index.246b7df3.js";import"./index.475368bd.js";import"./TotalPoints.52d039ba.js";const s=()=>o(r,{mt:2,children:o(t,{showHowMany:1e3})}),f=s;export{f as default};
