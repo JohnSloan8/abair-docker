079a6280c069614562eed758aa2ac8fabb012778
john <johnsloan1984@gmail.com>
2023-04-19 10:45:40 +0100

    added all returns to error check

--- error-check/dist/runErrorCheck.js
+++ error-check/dist/runErrorCheck.js
@@ -1,37 +1,28 @@
 // import { caolLeathanChecker } from "./caolLeathanChecker";
-import {
-  conjugationChecker,
-  fadaChecker,
-  typoChecker,
-  broadSlenderChecker,
-  stringifyConjugation,
-  precedingWordChecker,
-} from "./errorCheckers/index.js";
+import { conjugationChecker, fadaChecker, typoChecker, broadSlenderChecker, stringifyConjugation, precedingWordChecker, } from "./errorCheckers/index.js";
 const runErrorCheck = (word, target, hints) => {
-  // This is where the five error functions are used and output is decided
-  console.log("hints:", hints);
-  const conjugationOutput = conjugationChecker(word, target);
-  let stringConjugationOutput = "null";
-  if (conjugationOutput != null) {
-    stringConjugationOutput = stringifyConjugation(
-      conjugationOutput.word,
-      conjugationOutput.target
-    );
-  }
-  const fadaOutput = fadaChecker(word, target);
-  const typoOutput = typoChecker(word, target);
-  const broadSlenderOutput = broadSlenderChecker(word, target);
-  const precedingWordOutput = precedingWordChecker(word, target);
-  const data = {
-    word: word,
-    target: target,
-    conjugationOutput: conjugationOutput,
-    fadaOutput: fadaOutput,
-    typoOutput: typoOutput,
-    broadSlenderOutput: broadSlenderOutput,
-    precedingWordOutput: precedingWordOutput,
-    stringConjugationOutput: stringConjugationOutput,
-  };
-  return data;
+    // This is where the five error functions are used and output is decided
+    console.log("hints:", hints);
+    const conjugationOutput = conjugationChecker(word, target);
+    let stringConjugationOutput = "null";
+    if (conjugationOutput != null) {
+        stringConjugationOutput = stringifyConjugation(conjugationOutput.word, conjugationOutput.target);
+    }
+    const fadaOutput = fadaChecker(word, target);
+    const typoOutput = typoChecker(word, target);
+    const broadSlenderOutput = broadSlenderChecker(word, target);
+    const precedingWordOutput = precedingWordChecker(word, target);
+    const data = {
+        word: word,
+        target: target,
+        conjugationOutput: conjugationOutput,
+        fadaOutput: fadaOutput,
+        typoOutput: typoOutput,
+        broadSlenderOutput: broadSlenderOutput,
+        precedingWordOutput: precedingWordOutput,
+        stringConjugationOutput: stringConjugationOutput,
+    };
+    console.log("data:", data);
+    return data;
 };
 export default runErrorCheck;
