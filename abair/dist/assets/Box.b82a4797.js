caaa3ab53a311f3e8ece07cc89534eabcd286c96
john <johnsloan1984@gmail.com>
2023-05-09 14:52:20 +0100

    translations done

--- abair/dist/assets/Box.b82a4797.js
+++
@@ -1,1 +0,0 @@
-import{af as o}from"./App.d2165acb.js";const t=o(),r=t;export{r as B};
--- abair/dist/assets/Download.436c5421.js
+++
@@ -1,1 +0,0 @@
-import{r,i as a,j as t}from"./App.d2165acb.js";var e={},o=a.exports;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,v=o(r()),d=t,l=(0,v.default)((0,d.jsx)("path",{d:"M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"}),"Download");u=e.default=l;export{u as d};
